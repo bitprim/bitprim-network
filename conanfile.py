e21413280f10642235fd55f64cb285b8da9a8943
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-03-13 10:50:20 -0300

    add conan_req_version to the exported files

--- conanfile.py
+++ conanfile.py
@@ -24,11 +24,14 @@ from conans.model.version import Version
 
 def option_on_off(option):
     return "ON" if option else "OFF"
-
-def get_content(path):
+    
+def get_content(file_name):
     # print(os.path.dirname(os.path.abspath(__file__)))
     # print(os.getcwd())
-    with open(path, 'r') as f:
+    # with open(path, 'r') as f:
+    #     return f.read()
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)
+    with open(file_path, 'r') as f:
         return f.read()
 
 def get_version():
@@ -66,7 +69,7 @@ class BitprimNetworkConan(ConanFile):
         # "with_litecoin=False", \
 
     generators = "cmake"
-    exports = "conan_channel", "conan_version"
+    exports = "conan_channel", "conan_version", "conan_req_version"
     exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "bitprim-networkConfig.cmake.in", "bitprimbuildinfo.cmake", "include/*", "test/*"
     package_files = "build/lbitprim-network.a"
     build_policy = "missing"
